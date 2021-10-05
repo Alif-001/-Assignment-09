ddd778db55569f942e440e7ccc11c9d696015168
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2021-10-05 17:45:45 +0600

    done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,21 +4,20 @@ import aboutimg from "../../image/Designer _Flatline.png";
 
 const About = () => {
   return (
-    <div className="container">
-      {" "}
+    <div className="">
       <h1 className="mt-5">About us </h1>
       <div className="d-flex justify-content-center ">
-        <hr className="bg-info w-50 mx-5 px-5 align-items-center" />
+        <hr className="bg-info w-25 mx-5 px-5 d-flex  align-items-center" />
       </div>
-      <div className="">
-        <div className="d-flex w-100  ms-5">
-          <div className="ms-5">
+      <div xs={6} className=" container w-75 col-sm-12 ">
+        <div className="d-flex w-100  col-sm-12 ms-5">
+          <div className="ms-5 container col">
             <img src={aboutimg} alt="" className="" />
           </div>
-          <div className="ms-5 ps-5 ">
+          <div xs={6} className=" d-flex ms-5 ps-5 col">
             <div className="d-flex flex-column w-75">
-              <div id="about" className="align-items-center ">
-                <div className="  my-5   text-start">
+              <div id="about" className="align-items-center container ">
+                <div className="  my-5  col-sm-12  text-start">
                   <h2>We are E-learning.</h2>
                   <p>
                     An online learning community At E-learning, we believe
@@ -28,7 +27,7 @@ const About = () => {
                     experts.
                   </p>
                 </div>
-                <div className="my-5 text-start">
+                <div className="my-5 text-start  col-sm-12">
                   <h4>A leadership team with vision</h4>
                   <p>
                     Our executives lead by example and guide us to accomplish
